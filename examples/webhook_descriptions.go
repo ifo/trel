a5f9d8b42093999c1304fec5421ebf817b100503
Steve McCarthy <steve@redlua.com>
2019-01-01 00:27:15 -0500

    Add webhook fetch example

---
+++ examples/webhook_descriptions.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/ifo/trel"
+)
+
+func main() {
+	username := flag.String("u", "username", "your trello username or id")
+	apiKey := flag.String("k", "apikey", "your trello api key")
+	token := flag.String("t", "token", "your token - https://trello.com/app-key")
+	flag.Parse()
+
+	client := trel.New(
+		*username, // Your username
+		*apiKey,   // Your api key
+		*token,    // Your token (https://trello.com/app-key)
+	)
+
+	webhooks, err := client.Webhooks()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(webhooks) == 0 {
+		fmt.Println("No webhooks found")
+		return
+	}
+	for _, webhook := range webhooks {
+		fmt.Println(webhook.Description)
+	}
+}
