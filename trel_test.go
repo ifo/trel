02be8ce87ea01178ed2e89c3956313d7bcd03d34
Steve McCarthy <steve@redlua.com>
2019-01-30 10:45:56 -0500

    Add test for Client_Boards method

---
+++ trel_test.go
@@ -0,0 +1,42 @@
+package trel
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func setupClientMuxServer() (*Client, *http.ServeMux, *httptest.Server) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	client := New(server.Client(), "", "")
+	client.BaseURL, _ = url.Parse(server.URL)
+	return client, mux, server
+}
+
+func TestClient_Boards(t *testing.T) {
+	client, mux, server := setupClientMuxServer()
+	defer server.Close()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `[{"name": "Test", "id": "111122223333444455556666"}]`)
+	})
+
+	boards, err := client.Boards("Username")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(boards) != 1 {
+		t.Errorf("Expected 1 Board, got %d boards\n", len(boards))
+	}
+
+	if boards[0].Name != "Test" {
+		t.Errorf("Expected \"Test\", got %q\n", boards[0].Name)
+	}
+	if boards[0].ID != "111122223333444455556666" {
+		t.Errorf("Expected \"111122223333444455556666\", got %q\n", boards[0].ID)
+	}
+}
